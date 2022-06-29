0fcce0b12103cfa8baf2c9373cd978dee8e36f20
SagarBHiranna <100116539+SagarBHiranna@users.noreply.github.com>
2022-06-29 21:42:14 +0530

    Add files via upload

---
+++ analogToDigitalConverter.c
@@ -0,0 +1,36 @@
+#include "analogToDigitalConverter.h"
+
+int getMaxSensorDigitalValue(int A2DResolution)
+{
+	return (pow(2,A2DResolution) - 2);
+}
+
+int digitalToAnalogCurrentValue(int A2DValues, int A2DResolution, int maxCurrentValue)
+{	
+	float currentValue;
+	int analogConvertedvalue;
+	currentValue = (maxCurrentValue * ((A2DValues) / (getMaxSensorDigitalValue(A2DResolution)))) / (getMaxSensorDigitalValue(A2DResolution));
+	analogConvertedvalue = round(currentValue);
+	if(analogConvertedvalue < 0)
+		analogConvertedvalue = abs(analogConvertedvalue);
+    return analogConvertedvalue;	
+}
+
+int chargingCurrentValue(int A2DValues[], int numberOfSamples, int A2DResolution, int maxCurrentValue, int* analogValues)
+{
+	int invalidValue;
+	invalidValue = (pow(2,A2DResolution) - 1);
+    //int digitalValues[] = {};
+	for (int i=0; i< numberOfSamples; i++)
+	{
+		if(A2DValues[i] != invalidValue)
+		{
+			analogValues[i] = digitalToAnalogCurrentValue(A2DValues[i], A2DResolution, maxCurrentValue);
+		}
+		else{
+			return 0;
+		}
+		return 1;
+	}
+	
+}
